f9c964ac12b529701f3bcf2eacebc58f1f923548
Sumin Kim <ksumin4022@naver.com>
2026-07-15 17:50:25 +0900

    practice : 산술연산자

---
+++ 06_operator_update_prac.py
@@ -0,0 +1,26 @@
+# == 실습 1 : 사칙연산 계산기 ==
+print("== 실습 1 : 사칙연산 계산기 ==")
+a = 123
+b = 23
+
+print(a, "+", b, "=", a + b)
+print(a, "-", b, "=", a - b)
+print(a, "*", b, "=", a * b)
+print(a, "/", b, "=", a / b)
+print(a, "//", b, "=", a // b)
+print(a, "%", b, "=", a % b)
+print(a, "**", b, "=", a**b)
+
+# == 실습 2 : 평균과 도형 계산 ==
+print("== 실습 2 : 평균과 도형 계산 ==")
+width = 30
+length = 20
+height = 55
+print(
+    "평균 =",
+    (width + length + height) / 3,
+    "넓이 =",
+    width * length,
+    "부피 =",
+    width * length * height,
+)

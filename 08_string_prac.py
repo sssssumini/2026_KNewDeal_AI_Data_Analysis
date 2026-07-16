a4aa9ad7bf0ac7a51f7c32150277985537fb6b77
Sumin Kim <ksumin4022@naver.com>
2026-07-16 15:30:02 +0900

    practice : 이스케이프 문자

---
+++ 08_string_prac.py
@@ -0,0 +1,9 @@
+# == 실습 : 설비 정보 출력 카드 만들기 ==
+print("== 실습 : 설비 정보 출력 카드 만들기 ==\n")
+
+code = "PUMP_A"
+state = "정상"
+time = int(1200)
+check = "2026-07-16"
+
+print(f"설비: {code}\n상태: {state}\n가동: {time}\n점검: {check}")
